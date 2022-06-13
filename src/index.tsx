c665ea75b573cb82eaf3b48bdd33369c54095a50
Semyon Zasov <semenmaslina@gmail.com>
2022-06-13 13:14:26 +0300

    добавил redux + hook для типизации

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { storeSetup } from "./redux/index";
+import { Provider } from "react-redux";
+const store = storeSetup();
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
-
-
